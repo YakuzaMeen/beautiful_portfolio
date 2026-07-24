5d80654844566f76e0b126420bd11c817bfffacb
Oskar Rodrigo Sosa Soto <144083204+YakuzaMeen@users.noreply.github.com>
2026-07-24 00:08:11 -0500

    style: load extended portfolio styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// importar tu Home, no el icono
+import "./portfolio.css";
 import { Home } from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
